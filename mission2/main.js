a9e0aec0dbc36bd605f1a7f88c0c4d25c026f37a
gaoyifang <drakedoglife@outlook.com>
2018-09-06 23:19:39 +0800

    yesss

--- mission2/main.js
+++ mission2/main.js
@@ -1,39 +1,10 @@
-Vue.component('todo-item', {
-    props: ['todo'],
-    template: '<li>{{todo.text}}</li>'
-})
-
 var app = new Vue({
     el: '#app',
     data: {
-        foods: [
-            {id: 0, text: '蔬菜'},
-            {id: 1, text: '奶酪'},
-            {id: 3, text: '汉堡'}
-        ]
+        ok: true,
+        message: 'message'
     }
 })
 
-var vm = new Vue({
-    el: '#demo',
-    data: {
-        firstName: 'Foo',
-        lastName: 'Bar'
-    },
-    computed: {
-        fullName: {
-            // getter
-            get: function () {
-                return this.firstName + ' ' + this.lastName
-            },
-            // setter
-            set: function (newValue) {
-                var names = newValue.split(' ')
-                this.firstName = names[0]
-                this.lastName = names[names.length - 1]
-            }
-        }
-    }
 
-})
 
